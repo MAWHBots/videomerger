c593d50e849af2bf066abd0d29eadf0db886ee7a
MAWHBots <160210473+MAWHBots@users.noreply.github.com>
2024-08-14 15:10:59 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -4,26 +4,26 @@ import os
 
 
 class Config(object):
-    API_ID = os.environ.get("API_ID")
-    API_HASH = os.environ.get("API_HASH")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
+    API_ID = os.environ.get("API_ID", "23323912")
+    API_HASH = os.environ.get("API_HASH", "5b7decd292e78a57d631ea1849b7098a")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7212133770:AAE-mJVuPD6Cz59wcLthQWfIDIFMmFgRZEM")
     SESSION_NAME = os.environ.get("SESSION_NAME", "Video-Merge-Bot")
-    UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL")
-    LOG_CHANNEL = os.environ.get("LOG_CHANNEL")
+    UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "moviesandwebserieshubOrzz")
+    LOG_CHANNEL = os.environ.get("LOG_CHANNEL", "-1002207296876")
     DOWN_PATH = os.environ.get("DOWN_PATH", "./downloads")
     TIME_GAP = int(os.environ.get("TIME_GAP", 5))
-    MAX_VIDEOS = int(os.environ.get("MAX_VIDEOS", 5))
+    MAX_VIDEOS = int(os.environ.get("MAX_VIDEOS", 10))
     STREAMTAPE_API_USERNAME = os.environ.get("STREAMTAPE_API_USERNAME")
     STREAMTAPE_API_PASS = os.environ.get("STREAMTAPE_API_PASS")
-    MONGODB_URI = os.environ.get("MONGODB_URI")
+    MONGODB_URI = os.environ.get("MONGODB_URI", "mongodb+srv://dasaripardhasaradhi141:X5SstfxEbJORxOTh@cluster0.fvgzzwq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", False))
-    BOT_OWNER = int(os.environ.get("BOT_OWNER", 1445283714))
+    BOT_OWNER = int(os.environ.get("BOT_OWNER", 2036125193))
 
     START_TEXT = """
 Hi 👋, I am a Video Merge Bot!
 I can Merge Multiple Videos in One Video. Video Formats should be same.
 """
-    CAPTION = "Video Merged by @{}\n\nMade by @Astra_botz"
+    CAPTION = "Video Merged by @{}\n\nMade by @moviesandwebserieshubOrzz"
     PROGRESS = """
 Percentage : {0}%
 Done: {1}
